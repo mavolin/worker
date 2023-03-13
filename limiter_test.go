0f27604c8717b18c91e0c060f3586ae20414b993
mavolin <github@mavolin.co>
2023-03-13 09:21:33 +0100

    fix: invalid test

--- limiter_test.go
+++ limiter_test.go
@@ -33,29 +33,6 @@ func TestLimiter_Do(t *testing.T) {
 	assert.Equal(t, expect150, actual.Load())
 }
 
-func TestLimiter_Go(t *testing.T) {
-	t.Parallel()
-
-	l := NewLimiter(5)
-
-	expect50 := int32(5)
-	expect150 := int32(10)
-	var actual atomic.Int32
-
-	for i := 0; i < int(expect150); i++ {
-		l.Go(func() {
-			actual.Add(1)
-			time.Sleep(100 * time.Millisecond)
-		})
-	}
-
-	time.Sleep(50 * time.Millisecond)
-	assert.Equal(t, expect50, actual.Load())
-
-	time.Sleep(100 * time.Millisecond)
-	assert.Equal(t, expect150, actual.Load())
-}
-
 func TestLimiter_Try(t *testing.T) {
 	t.Parallel()
 
